a8aca3d1d71038b2656c92df04f0ef6cbab0c31e
Hargun-singhh <hargunsingh14.hs@gmail.com>
2025-03-04 02:30:45 +0530

    Changes

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -2,11 +2,14 @@ const students = require('../data/students');
 
 
 exports.allstudents = (req,res) =>{
+    if (students.length === 0) {
+        return res.status(404).json({ message: "No students found" });
+    }
     res.status(200).json(students);
 };
 
 exports.addstudent = (req,res) =>{
     const data = req.body;
     students.push(data);
-    res.status(201).json({"Status":"Student Record Added Successfully :) "});
+    res.status(201).json({message: "Student Record Added Successfully :) "});
 };
