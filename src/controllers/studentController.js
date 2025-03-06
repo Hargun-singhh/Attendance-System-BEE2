86334b5396f226d059ca0478d4f8d7e1d4480c07
Hargun Deep Singh <hargunsingh14.hs@gmail.com>
2025-03-06 14:42:52 +0530

    Student Controller.js - Kashvi

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,5 +1,6 @@
 const students = require('../data/students');
 
+//Show all students
 exports.allstudents = (req, res) => {
     if (students.length === 0) {
         return res.status(404).json({ message: "No students found" });
@@ -7,6 +8,7 @@ exports.allstudents = (req, res) => {
     res.status(200).json(students);
 };
 
+//Add a new student
 exports.addstudent = (req, res) => {
     const {SName, RollNumber } = req.body;
 
@@ -19,4 +21,31 @@ exports.addstudent = (req, res) => {
 
     students.push(StundentDetails);
     res.status(201).json({ message: "Student Record Added Successfully :) " });
+};
+
+// Update Student Name
+exports.updateStudentName = (req, res) => {
+    const { roll_no } = req.params; 
+    const { name } = req.body; 
+    const student = students.find(s => s.roll_no === parseInt(roll_no));
+
+    if (!student) {
+        return res.status(404).json({ message: "Student not found" });
+    }
+
+    student.name = name;
+    res.status(200).json({ message: "Student name updated successfully", student });
+};
+
+// Delete Student record by its roll No
+exports.deleteStudentByRollNo = (req, res) => {
+    const { roll_no } = req.params;
+    const index = students.findIndex(student => student.roll_no === parseInt(roll_no));
+
+    if (index === -1) {
+        return res.status(404).json({ message: "No such student exists" });
+    }
+
+    students.splice(index, 1);
+    res.status(200).json({ message: "Student Record deleted successfully" });
 };
